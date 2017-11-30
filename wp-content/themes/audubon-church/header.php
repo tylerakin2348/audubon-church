17173a9b6f5e580a6086a1e98dc9eb19135e4a8a
Tyler Akin <takin963@students.sbts.edu>
2017-11-30 16:35:11 -0500

    changes

--- wp-content/themes/audubon-church/header.php
+++ wp-content/themes/audubon-church/header.php
@@ -24,11 +24,17 @@
             <a href='<?php echo esc_url( home_url( '/' ) ); ?>' title='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>' rel='home'><img src='<?php echo esc_url( get_theme_mod( 'themeslug_logo' ) ); ?>' alt='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>'></a>
         </div>
     <?php else : ?>
-        <hgroup>
-            <h1 class='site-title'><a href='<?php echo esc_url( home_url( '/' ) ); ?>' title='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>' rel='home'><?php bloginfo( 'name' ); ?></a></h1>
-            <h2 class='site-description'><?php bloginfo( 'description' ); ?></h2>
+        <?php if (is_front_page()) : ?>
+            <hgroup>
+                <h1 class='site-title'><a href='<?php echo esc_url( home_url( '/' ) ); ?>' title='The Church in Audubon Park' rel='home'>est. 1944</a></h1>
+            </hgroup>
 
-        </hgroup>
+        <?php else : ?>
+            <hgroup>
+                <h1 class='site-title'><a href='<?php echo esc_url( home_url( '/' ) ); ?>' title='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>' rel='home'><?php bloginfo( 'name' ); ?></a></h1>
+                <h2 class='site-description'><?php bloginfo( 'description' ); ?></h2>
+            </hgroup>
+        <?php endif; ?>
     <?php endif; ?>
 
     <nav class="main-navigation">
